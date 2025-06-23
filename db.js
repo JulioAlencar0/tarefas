db120f7b20f056e5648f7f2fe5beab062e8bd2ea
Júlio <julinhoalencar30@gmail.com>
2025-06-23 13:04:57 -0300

    add data pgsql

---
+++ db.js
@@ -0,0 +1,14 @@
+const { Pool } = require("pg"); //importando Pool do pg
+
+//Configuração da conexão com o banco PostgreSQL
+const pool = new Pool({
+  user: "postgres", //nome que está no PostgreSQL
+  host: "localhost", //host do banco
+  database: "tarefas", //nome do banco
+  password: "JCFA2711", //senha do postgreSQL
+  port: 5432, //porta padrão do PostgreSQL
+});
+
+pool.connect()
+  .then(() => console.log("Conectado ao PostgreSQL!"))
+  .catch((err) => console.log("Erro na conexão ao banco de dados", err));
